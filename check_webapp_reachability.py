ac3086fcf5a33ef4becda9d720853c68be98227f
wollow12 <62893839+wollow12@users.noreply.github.com>
2024-05-28 12:58:57 +0300

    Create check_webapp_reachability.py

---
+++ check_webapp_reachability.py
@@ -0,0 +1,19 @@
+import unittest
+import requests
+
+
+class TestWebsiteReachability(unittest.TestCase):
+    def test_website_reachable(self):
+        url = 'http://51.20.115.143/'
+
+        try:
+            response = requests.get(url)
+            # Checks if the status code is within the range of successful responses
+            self.assertTrue(response.status_code >= 200 and response.status_code < 300)
+        except requests.exceptions.RequestException as e:
+            self.fail(f"Failed to reach website: {e}")
+
+
+if __name__ == '__main__':
+    unittest.main()
+  
